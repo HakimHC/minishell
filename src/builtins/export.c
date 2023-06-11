5fc5171eb442a9392268abf04f452c7bffb51927
HakimHC <ahmedh90111@gmail.com>
2023-06-11 20:38:48 +0200

    export without args added

--- src/builtins/export.c
+++ src/builtins/export.c
@@ -6,19 +6,37 @@
 /*   By: hakim </var/spool/mail/hakim>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/01 18:30:22 by hakim             #+#    #+#             */
-/*   Updated: 2023/06/11 04:42:59 by hakahmed         ###   ########.fr       */
+/*   Updated: 2023/06/11 20:37:29 by hakahmed         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
 #include <stdlib.h>
 
+#include "libft.h"
 #include "minishell.h"
+
+void	export_noargs(void)
+{
+	t_list	*curr;
+	t_env	*env;
+
+	curr = g_data->envp;
+	while (curr)
+	{
+		env = curr->content;
+		printf("declare -x %s=\"%s\"\n", env->key, env->value);
+		curr = curr->next;
+	}
+}
 
 void	ft_export(t_list *args)
 {
 	t_list	*curr;
 
 	curr = args;
+	if (!curr)
+		return ((void) export_noargs());
 	while (curr)
 	{
 		if (!export_error(curr->content))
